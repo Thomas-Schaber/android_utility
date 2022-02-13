a833e369a6286ab61239542af35012a91734b8d0
Thomas-Schaber <tommy.schaber@gmail.com>
2022-02-13 16:30:07 -0500

    added methods to find packages on device

--- Adb_Util.py
+++ Adb_Util.py
@@ -5,6 +5,7 @@ Created on Wed Feb  2 09:49:33 2022
 @author: tommy
 """
 import subprocess
+import numpy as np
 
 class Adb_Util:
     #TODO start adb server
@@ -36,12 +37,43 @@ class Adb_Util:
         else:
             print("Failed to install apk: %s" % file, " with code %d" % install.returncode)
             return False
+        
+    # Pulls devices installed packages returns a numpy array or False
+    # adb shell cmd package list packages
+    # adb -s R38M40EX5ZA shell cmd package list packages
+    def package_list(self, device_id):
+        packages = np.array([])
+        packages_command = subprocess.run(['adb', '-s', device_id, 'shell', 'cmd', 'package', 'list', 'packages', '-3' ], capture_output=True, shell=True)
+
+        if packages_command.returncode == 0:
+            for line in packages_command.stdout.decode('utf-8').splitlines():
+                line = line.split('package:')
+                packages = np.append(packages, line[1])
+            print(packages)
+            print(len(packages))
+            return packages
+        else:
+            print(packages_command.stderr)
+            return False
+            
 
         
         
         
-        
+
 Adb_Util().start_server()
-Adb_Util().install_package('C:\\Users\\tommy\\Desktop\\Duolingolanguage.apk', 'RFCR71TEQWP')
+test_list = Adb_Util().package_list('A00000K580152200711')
+Adb_Util().package_list('R38M40EX5ZA')
+
+deviceID = 'A00000K580152200711'
+
+for package in test_list:
+    print(f'unistalling package: {package}')
+    uninstall = subprocess.run(['adb', '-s', deviceID, 'uninstall', package], capture_output=True, shell=True)
+    
+    
+
+#Adb_Util().package_list('A00000K580152200711')
+#Adb_Util().install_package('C:\\Users\\tommy\\Desktop\\Duolingolanguage.apk', 'R38M40EX5ZA')
 
 #Adb_Util().kill_server()

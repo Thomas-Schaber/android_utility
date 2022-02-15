ec4999d0617ed0f2b90c15d552b0ad1fce8d662d
Thomas-Schaber <tommy.schaber@gmail.com>
2022-02-14 21:08:02 -0500

    Utilized Adb_Util to write uninstall/install methods for Device.py

--- Adb_Util.py
+++ Adb_Util.py
@@ -30,14 +30,24 @@ class Adb_Util:
 
     # Installs file using adb command
     def install_package(self, file, deviceID):
-        install = subprocess.run(['adb', '-s', deviceID, 'install', file], capture_output=True, shell=True)
-        if install.returncode == 0:
+        install_command = subprocess.run(['adb', '-s', deviceID, 'install', file], capture_output=True, shell=True)
+        if install_command.returncode == 0:
             print("Package {:s} installed".format(file))
             return True
         else:
-            print("Failed to install apk: %s" % file, " with code %d" % install.returncode)
+            print("Failed to install apk: %s" % file, " with code %d" % install_command.returncode)
             return False
-        
+    
+    def unistall_package(self, deviceID, package_name):
+        print("Uninstalling", package_name)
+        uninstall_command = subprocess.run(['adb', '-s', deviceID, 'uninstall', package_name])
+        if uninstall_command.returncode == 0:
+            print(package_name, "uninstalled successfully\n")
+        else:
+            print(uninstall_command.stdout)
+            print("An error occured with package", package_name)
+    
+    
     # Pulls devices installed packages returns a numpy array or False
     # adb shell cmd package list packages
     # adb -s R38M40EX5ZA shell cmd package list packages
@@ -55,24 +65,58 @@ class Adb_Util:
         else:
             print(packages_command.stderr)
             return False
-            
+        
+        
+    def get_device_list(self):
+        
+        
+        devices = np.array([])
+        
+        get_device_command = subprocess.run(['adb', 'devices'], capture_output=True, shell=True)
+        
+        
+        if get_device_command.returncode == 0:
+            print("Getting device list:\n")
+            for line in get_device_command.stdout.decode('utf8').splitlines():
+                
+                if line.__contains__('List'):
+                    continue
+                else:
+                    line = line.split('\t')
+                    if line.__contains__('offline'):
+                        print("Device", line[0], "is offline")
 
+                    if line[0] != '':
+                        devices = np.append(devices, line[0])
+                
+        else:
+            print("An error occured when running get devices:", get_device_command.stderr)
+            return get_device_command.returncode
+        if len(devices) == 0:
+            print("Device list is empty...")
+
+        return devices 
         
         
         
 
-Adb_Util().start_server()
-test_list = Adb_Util().package_list('A00000K580152200711')
-Adb_Util().package_list('R38M40EX5ZA')
+#Adb_Util().start_server()
 
-deviceID = 'A00000K580152200711'
+#device_list = Adb_Util().get_device_list()
+#for device in device_list:
+#    Adb_Util().package_list(device)
 
-for package in test_list:
-    print(f'unistalling package: {package}')
-    uninstall = subprocess.run(['adb', '-s', deviceID, 'uninstall', package], capture_output=True, shell=True)
+#test_list = Adb_Util().package_list('A00000K580152200711')
+#Adb_Util().package_list('R38M40EX5ZA')
+
+#deviceID = 'A00000K580152200711'
+
+#for package in test_list:
+ #   print(f'unistalling package: {package}')
+  #  uninstall = subprocess.run(['adb', '-s', deviceID, 'uninstall', package], capture_output=True, shell=True)
     
     
-
+#Adb_Util().get_device_list()
 #Adb_Util().package_list('A00000K580152200711')
 #Adb_Util().install_package('C:\\Users\\tommy\\Desktop\\Duolingolanguage.apk', 'R38M40EX5ZA')
 
--- Device.py
+++ Device.py
@@ -5,7 +5,7 @@ Created on Wed Feb  2 09:30:32 2022
 @author: tommy
 """
 import numpy as np
-
+from Adb_Util import Adb_Util
 
 class Device:
     def __init__(self, name, deviceID, screenDensity, sdk, os):
@@ -14,17 +14,33 @@ class Device:
         self.screenDensity = screenDensity
         self.sdk = sdk
         self.os = os
-        self.packageList = np.array(5, dtype='S') 
+        self.packageList = np.array([], dtype='S') 
         
 
-    def init_package_list():
-        return 0
+    def init_package_list(self):
+        self.packageList = Adb_Util().package_list(self.deviceID)
 
-    def install():
-        return 0
+    def install(self, file):
+        Adb_Util().install_package(file, self.deviceID)
     
     
-    def uninstall():
-        return 0
+    def uninstall_all(self):
+        if len(self.packageList) > 0:
+            for package_name in self.packageList:
+                Adb_Util().unistall_package(self.deviceID, package_name)
+        else:
+            print("The amount of apps installed on device is currently 0")
+            
+    def uninstall(self, package_name):
+        if len(self.packageList) > 0:
+            Adb_Util().unistall_package(self.deviceID, package_name)
+        else:
+            print("The amount of apps installed on device is currently 0")
+    
+    def get_packageList(self):
+        return self.packageList
 
-d1 = Device('Fold', '123123123', 'hdmix', 30, 'Go')
+d1 = Device('Nokia', 'A00000K580152200711', 'hdmix', 30, 'Go')
+d1.init_package_list()
+print(d1.packageList)
+d1.uninstall_all()
